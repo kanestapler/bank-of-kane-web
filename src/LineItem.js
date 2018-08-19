bc1e556cb898f00d66642e1d8d8087c00bba9e98
kanestapler <kanestapler@gmail.com>
2018-08-19 14:28:11 -0700

    Starting to add firebase

---
+++ src/LineItem.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './LineItem.css';
+
+class LineItem extends Component {
+    render() {
+        return (
+            <div className="LineItem">
+                Hello
+            </div>
+        );
+    }
+}
+
+export default LineItem;
