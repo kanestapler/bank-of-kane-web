9f1a4daa1e2e5fe09420d6d979825b01bf43cb2b
kanestapler <kanestapler@gmail.com>
2018-08-19 19:14:02 -0700

    routing added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
 import Outstanding from './Outstanding';
+import Admin from './Admin'
+import { Link, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Outstanding/>
+        <nav>
+          <Link className="nav-link" to="/">Home</Link>
+          <Link className="nav-link" to="/admin">Admin</Link>
+        </nav>
+        <div>
+          <Route exact path="/" component={Outstanding} />
+        </div>
+        <div>
+          <Route path="/admin" component={Admin} />
+        </div>
       </div>
     );
   }
